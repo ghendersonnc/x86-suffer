4e9c7e4a9f6810680fca3e5c1a478a9131d5ba07
ghendersonnc <ghendersonnc@gmail.com>
2022-06-04 21:52:37 -0400

    negative numbers

--- main.asm
+++ main.asm
@@ -1,8 +1,7 @@
 section .data
-dividend dd 1200
+dividend dd -1200
 divisor dd 1000
 remainder dd 0
-quotient dd 0
 
 section .text
 global _start
@@ -11,35 +10,52 @@ _start:
     mov ecx, [divisor]
 
     WHILE_DIVISOR_ABOVE_ZERO:
+        cmp DWORD [dividend], -1
+        jle display_negative
         cmp DWORD [divisor], 0
         jg display_number
         jmp end
 
+        display_negative:
+            mov eax, 0x2D
+            push eax
+            lea ecx, [esp]
+            add esp, 4
+
+            mov eax, 0x4
+            mov ebx, 0x1
+            mov edx, 1
+            int 0x80
+
+            neg DWORD [dividend]
+
+            mov eax, [dividend]
+            mov ecx, [divisor]
+
         display_number:
-        mov edx, 0
-        div ecx
+            mov edx, 0
+            div ecx
 
-        mov DWORD [quotient], eax
-        mov DWORD [remainder], edx
+            mov DWORD [remainder], edx
 
-        add eax, 0x30
-        push eax
-        lea ecx, [esp]
-        add esp, 4
+            add eax, 0x30
+            push eax
+            lea ecx, [esp]
+            add esp, 4
 
-        mov eax, 0x4
-        mov ebx, 0x1
-        mov edx, 1
-        int 0x80
+            mov eax, 0x4
+            mov ebx, 0x1
+            mov edx, 1
+            int 0x80
 
-        mov edx, 0
-        mov eax, [divisor]
-        mov ecx, 10
-        div ecx
+            mov edx, 0
+            mov eax, [divisor]
+            mov ecx, 10
+            div ecx
 
-        mov ecx, eax
-        mov DWORD [divisor], eax
-        mov eax, [remainder]
+            mov ecx, eax
+            mov DWORD [divisor], eax
+            mov eax, [remainder]
 
     jmp WHILE_DIVISOR_ABOVE_ZERO
 
